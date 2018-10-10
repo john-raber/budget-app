925a40afa06eb71623d27dd5025e099e9c1f1654
john-raber <jrab224@gmail.com>
2018-10-10 16:51:58 -0400

    setting up user and budget resource, creating and migrating database, seeding users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = [
+  'volcanicbadger',
+  'grubblingwalrus'
+]
+
+users.each do |u|
+  User.create({ username: u })
+end
