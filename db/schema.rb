2195b144ebde46fdcbb600fdb272d1b205c4d7a8
john-raber <jrab224@gmail.com>
2018-10-10 18:44:51 -0400

    tested post fetch with postman and able to create users with password, updated db migration for password digest, cleared extra routes, added create controller action

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_10_10_202922) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-users = [
-  'volcanicbadger',
-  'grubblingwalrus'
-]
-
-users.each do |u|
-  User.create({ username: u })
-end
+# users = [
+#   { username: 'volcanicbadger' }
+#   'grubblingwalrus'
+# ]
+#
+# users.each do |u|
+#   User.create({ username: u })
+# end
