0b45ef006e3306f933a7a99724c8b2d1c2ec6a43
john-raber <jrab224@gmail.com>
2018-10-11 10:22:46 -0400

    added email field to user model, updated controller and migration

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_10_10_202922) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
