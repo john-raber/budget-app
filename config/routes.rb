0281b6a7d476c5cfbb08924d8fcc7468f68474bf
john-raber <jrab224@gmail.com>
2018-10-11 14:18:40 -0400

    updated controllers for versioning;

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  # resources :budgets
-  resources :users, only: [:create]
-  post '/login', to: 'auth#create'
-  get '/profile', to: 'users#profile'
+  namespace :api do
+    namespace :v1 do
+      # resources :budgets
+      resources :users, only: [:create, :index]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
