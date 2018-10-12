a7c6a412034a69c1a06e00cbdc2bf26f0d238ce1
john-raber <jrab224@gmail.com>
2018-10-12 11:08:25 -0400

    removing unnecessary show route in users controller

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,9 +1,5 @@
 class Api::V1::AuthController < ApplicationController
-  skip_before_action :authorized, only: [:create, :show]
-
-  def show
-
-  end
+  skip_before_action :authorized, only: [:create]
 
   def create
     @user = User.find_by(username: user_login_params[:username])
