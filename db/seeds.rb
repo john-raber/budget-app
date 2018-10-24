42db46551abbfef0cf60dcfb5c5384145a879916
john-raber <jrab224@gmail.com>
2018-10-24 14:45:44 -0400

    adding transaction seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'csv'
+
 users = [
   'john',
   'volcanicbadger',
@@ -37,24 +39,31 @@ Budget.create([
 
 accounts = [
   'main savings',
-  'buried gold bars',
-  'zombie apocalypse fund',
   'credit card'
 ]
 
 Account.create({ nickname: accounts[0], budget_id: 1 })
 Account.create({ nickname: accounts[1], budget_id: 1 })
-Account.create({ nickname: accounts[2], budget_id: 2 })
-Account.create({ nickname: accounts[2], budget_id: 1 })
-Account.create({ nickname: accounts[3], budget_id: 3 })
+Account.create({ nickname: accounts[0], budget_id: 2 })
+Account.create({ nickname: accounts[0], budget_id: 3 })
 Account.create({ nickname: accounts[1], budget_id: 3 })
-Account.create({ nickname: accounts[2], budget_id: 4 })
+Account.create({ nickname: accounts[0], budget_id: 4 })
 Account.create({ nickname: accounts[1], budget_id: 4 })
 
 categories = [
   {
     name: 'food',
     balance: 200.0,
+    budget_id: 1
+  },
+  {
+    name: 'videogames',
+    balance: 150.0,
+    budget_id: 1
+  },
+  {
+    name: 'gas',
+    balance: 60.0,
     budget_id: 1
   },
   {
@@ -91,4 +100,35 @@ categories = [
 
 categories.each do |c|
   Category.create(c)
+end
+
+CSV.foreach('./db/OctoberTransactions.csv', headers: true) do |row|
+  # example row: POSTED,10/06/2018,10/08/2018,8405,DOMINO'S 4340,Dining,20.29,
+
+  # based on what the category is from the CSV data, I want to associate the
+  # transaction with a category that has already been created by giving the new
+  # transaction a different category_id
+
+  case row[5]
+  when 'Dining' || 'Grocery'
+    category_id = 1
+  when 'Internet'
+    category_id = 2
+  when 'Gas/Automotive'
+    category_id = 3
+  when 'Utilities'
+    category_id = 4
+  end
+
+  transaction = {
+    name: row[4],
+    description: row[4],
+    date: row[1],
+    amount: row[6],
+    transaction_type: 'expense',
+    account_id: 1,
+    category_id: category_id
+  }
+
+  Transaction.create(transaction)
 end
